bde671c19d06ae1361b375ce9ecfcaa5549af4aa
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-12-18 15:06:50 +0100

    Minor fix.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ int main(int argc, char* argv[])
 	if(argc <= 2)
 	{
 		std::cout << "Wrong arguments, please provide the .xml or .stream and the raw input file." << std::endl;
-		std::cout << "Example: ./packetToMessage rta_fadc_all.xml Aar.raw" << std::endl;
+		std::cout << "Example: " << argv[0] << " rta_fadc_all.xml Aar.raw" << std::endl;
 		return 1;
 	}
+	std::string conf_filename = argv[1];
+	std::string input_filename = argv[2];
 
-	RTA::CameraEventStreamer streamer(argv[1], argv[2], 10000);
+	RTA::CameraEventStreamer streamer(conf_filename, input_filename, 10000);
     streamer.addConnection(ZMQ_PUSH, "tcp://*:12345");
 	streamer.start();
 	streamer.join();
